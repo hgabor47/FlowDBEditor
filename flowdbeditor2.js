f8ed3628d34d5b38fbf57b7b03c3f87ab59a45b7
gabor horvath <hgabor47@gmail.com>
2018-06-18 16:04:07 +0200

    Speech: colors and fields

--- flowdbeditor2.js
+++ flowdbeditor2.js
@@ -377,27 +377,30 @@ var TTable = function(name){
   };
 
   this.Selected=function(){
+    if (SelectedTable==this) return;
     if (SelectedTable!=null) {
       SelectedTable.noSelected();  //table  
     }
     SelectedTable=this;
-    SelectedField=null;
-    this.setReadOnly(this.readonly);
-    //this.DOMrect.setAttribute("style",selectedStyle);
+    if ((SelectedField!=null)&&(SelectedField.table!=SelectedTable)) {
+      SelectedField=null;
+    }
+    this.setReadOnly(this.readonly);    
     refreshFieldsListDOM();
   };
   this.noSelected=function(){
     SelectedTable=null;
     SelectedField=null;
-    this.setReadOnly(this.readonly);
-    //this.DOMrect.setAttribute("style",noSelectedStyle);
+    this.setReadOnly(this.readonly);    
     refreshFieldsListDOM();
   };
 
   this.edit=function(parent){
     if (stateEdit) return;
     stateEdit=true;
+    commandgroup=2;
     var div = document.createElement("div");
+    div.setAttribute("id","flow_edit");
     div.className="flow_edit";
     div.style.top=Number(this.posxy[1]+20)+"px";
     div.style.left=Number(this.posxy[0]-30)+"px";
@@ -416,7 +419,7 @@ var TTable = function(name){
       div.innerHTML+=`<input type="checkbox" id="edit_readonly" >`;
     div.innerHTML+=`<br>
      <button onclick="{commandgroup=0;editTableOK(this);}">OK</button>
-     <button onclick="{commandgroup=0;editTableCancel(this);}">Cancel</button>
+     <button onclick="{commandgroup=0;editCancel(this);}">Cancel</button>
      <button onclick="{commandgroup=0;editTableDelete(this);}">Delete</button>
     `;
     div.table=this;
@@ -433,6 +436,7 @@ var TTable = function(name){
     div.setAttribute("id","list");
     parent.appendChild(div);
     this.Selected();
+    commandgroup=1;
     list( ATables.indexOf(this),"list");
 
     div.table=this;
@@ -724,7 +728,9 @@ var TField = function(table,name){
   this.edit=function(parent){
     if (stateEdit) return;
     stateEdit=true;
+    commandgroup=3;
     var div = document.createElement("div");
+    div.setAttribute("id","flow_edit");
     div.className="flow_edit";    
     div.style.top=Number(this.table.posxy[1]+20)+"px";
     div.style.left=Number(this.table.posxy[0]-30)+"px";
@@ -765,12 +771,12 @@ var TField = function(table,name){
         <label>Filter by</label><input type="text" id="edit_linkfilterfield" value="`+this.linkfilter[3]+`">
         </span>`;        
      }
-     div.innerHTML+=`<hr><button onclick="editFieldOK(this)">OK</button>
-     <button onclick="editFieldCancel(this)">Cancel</button>`;
+     div.innerHTML+=`<hr><button onclick="{commandgroup=0;editFieldOK(this);}">OK</button>
+     <button onclick="{commandgroup=0;editCancel(this);}">Cancel</button>`;
     div.field=this;
     parent.appendChild(div);
     setFocus("edit_name");
-       
+    SelectedField=this;   
     if (this.link!=null) {
       changeCHKfilter(document.getElementById("edit_linkfilter"));
     }
@@ -1171,9 +1177,9 @@ function titleClick(e){
 }
 
 function fieldClick(e){
-  if (flowMode==FlowModes.Flow){
-    SelectedField=this.field;
+  if (flowMode==FlowModes.Flow){    
     this.field.edit(document.getElementById("area"));
+    SelectedField=this.field;
   }
   if (flowMode==FlowModes.Constraint){
     if (constraintField==null){ //startpoint
@@ -1217,7 +1223,7 @@ function editTableOK(div){
   refreshTablesListDOM();  
   Save(temp);
 }
-function editTableCancel(div){
+function editCancel(div){
   removePanelDOM(div);
     
 }
@@ -1357,10 +1363,10 @@ function editFieldOK(div){
   refreshFieldsListDOM();
   Save(temp);
 }
+/*
 function editFieldCancel(div){
   removePanelDOM(div);
-
-}
+}*/
 function editFieldDelete(div){
   if (confirm("DELETE ONE FIELD! Sure?")) {    
     var panel = div.parentElement;
@@ -1508,6 +1514,41 @@ function setFocus(DOMname){
       foc.select();
     }, 5);  
 }
+
+
+function colourNameToHex(colour)
+{
+    var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
+    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
+    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
+    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
+    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
+    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
+    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
+    "honeydew":"#f0fff0","hotpink":"#ff69b4",
+    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
+    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
+    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
+    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
+    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
+    "navajowhite":"#ffdead","navy":"#000080",
+    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
+    "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
+    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
+    "violet":"#ee82ee",
+    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
+    "yellow":"#ffff00","yellowgreen":"#9acd32"};
+
+    if (typeof colours[colour.toLowerCase()] != 'undefined')
+        return colours[colour.toLowerCase()];
+
+    return false;
+}
+
 function getEditDistance2(p,cmd)  //(part,cmd) ->  [dist,chars,newcmd,%param value]
 { var res=[0,0,0,0];                    // nevezd át a táblát%nevűre > 
                                   // 1. nevezd át a táblát,nevezd át a táblát almafa névre
@@ -2057,7 +2098,7 @@ function list( tableidx , divname ){   // tomb.... és "lista"  a div id-je
   div2.className="flow_browse_header";
 
   div2.innerHTML=`<button onclick="list_new(`+tableidx+`)">NewRecord</button>    
-  <button onclick="{commandgroup=0;editTableCancel(this.parentElement);}">Exit</button><button onclick="editTableClear(`+tableidx+`)">ClearRecords</button>`;
+  <button onclick="{commandgroup=0;editCancel(this.parentElement);}">Exit</button><button onclick="editTableClear(`+tableidx+`)">ClearRecords</button>`;
   var table = ATables[tableidx];
   tomb=table.Records;
 
@@ -2554,7 +2595,9 @@ var commands = [
   ],
   [ ["enData entry commands samples","huAdatfelvételi parancs példák"],
     new TCMD(1000,"új sor"),new TCMD(1000,"new record","en"),new TCMD(1000,"új rekord"),
-    new TCMD(1001,"exit","en"),new TCMD(1001,"kilépés")
+    new TCMD(1001,"exit","en"),new TCMD(1001,"kilépés"),
+    new TCMD(1002,"scroll down","en"),new TCMD(1002,"scroll","en"),new TCMD(1002,"lapozz"),new TCMD(1002,"lapozz le"),new TCMD(1002,"lapozz előre"),
+    new TCMD(1003,"scroll up","en"),new TCMD(1002,"lapozz vissza"),new TCMD(1002,"lapozz fel"),
   ],
   [ ["enTable's datasheet commands samples","huTábla adatlap példák"],
     new TCMD(2000,"tablename","en"),new TCMD(2000,"táblanév"),new TCMD(2000,"tábla neve"),
@@ -2564,12 +2607,26 @@ var commands = [
     new TCMD(2004,"description","en"),new TCMD(2004,"leírás"),
     new TCMD(2005,"readonly","en"),new TCMD(2005,"legyen csak olvasható"),new TCMD(2005,"csak olvasható"),
     new TCMD(2006,"writeable","en"),new TCMD(2006,"legyen írható"),new TCMD(2006,"írható is"),
+    new TCMD(1001,"exit","en"),new TCMD(1001,"kilépés")
+  ],
+  [ 
+    ["enField's datasheet commands samples","huAdatmező adatlap példák"],
+    new TCMD(3000,"fieldname","en"),new TCMD(3000,"mezőnév"),new TCMD(3000,"mező neve"),
+    new TCMD(3001,"length","en")   ,new TCMD(3001,"hossz"),new TCMD(3001,"hosszúság"),
+    new TCMD(3002,"type","en")     ,new TCMD(3002,"type of field","en"),new TCMD(3002,"típus"),
+    new TCMD(3004,"description","en"),new TCMD(3004,"leírás"),
+    new TCMD(3005,"display","en")  ,new TCMD(3005,"megjelenítés"),new TCMD(3005,"jelenjen meg"),new TCMD(3005,"jelenjen meg listában"),
+    new TCMD(3006,"no display","en"),new TCMD(3006,"ne jelenjen meg"),new TCMD(3006,"ne jelenjen meg listában"),
+    new TCMD(3007,"delete connect","en"),new TCMD(3007,"delete link","en"),new TCMD(3007,"töröld a kapcsolatot"),new TCMD(3007,"kapcsolat törlése"),
+    new TCMD(3008,"delete field","en"),new TCMD(3008,"töröld a mezőt"),new TCMD(3008,"mező törlése"),
+    new TCMD(1001,"exit","en"),new TCMD(1001,"kilépés")
   ]
 ];
-var change = [["igen","yes"],["nem","no"],["free","3"],["form","4"],["cleared","create"],["tree","3"],["for","4"],["hive","5"],["one","1"],["two","2"],["too","2"],["six","6"],["sex","6"],
- ["84 bus","8 tables"],["timetables","10 tables"],["grade","create"],["portable","4 tables"],["turntables","10 tables"],["neighbour","table"],["you","new"],
+var change = [[" igen "," yes "],[" nem "," no "],[" free "," 3 "],[" form "," 4 "],["cleared","create"],[" tree "," 3 "],[" hive ","5"],[" one ","1"],[" two ","2"],[" too ","2"],[" six ","6"],[" sex ","6"],
+ ["84 bus","8 tables"],["timetables","10 tables"],["grade","create"],["portable","4 tables"],["turntables","10 tables"],["neighbour","table"],[" you ","new"],
  [" egy "," 1 "],["névtáblát","4 táblát"],[" két "," 2 "],[" kettő "," 2 "],[" négy "," 4 "],[" három "," 3 "],[" öt "," 5 "],[" hat "," 6 "],[" hét "," 7 "],[" nyolc "," 8 "],[" kilenc "," 9 "],[" tíz "," 10 "],
- ["logikai","boolean"],["szám","integer"],["stream","string"],["sztring","string"],["dátum","date"],["audi","id"],["díj","id"],["agy","adj"]
+ ["logikai","boolean"],[" szám "," integer "],["stream","string"],["sztring","string"],["dátum","date"],["audi","id"],["díj","id"],["agy","adj"],
+ ["piros","red"],["kék","blue"],["zöld","green"],["sárga","yellow"],["fekete","black"],["fehér","white"],["narancs","orange"],["szürke","gray"],["lila","purple"],["barna","brown"],["cián","cyan"],
  ];
 
 var TSPHistory = function(limit=20,timelimit=3000){ //pieces,ms
@@ -2586,7 +2643,7 @@ var TSPHistory = function(limit=20,timelimit=3000){ //pieces,ms
     this.time=(new Date()).getTime();
   };
   this.last=function() {
-    return this.verem[this.verem.length-1];
+    return this.verem[this.verem.length - 1 ];
   };
   this.ellapsed=function(){
     if ((this.time+timelimit)<(new Date()).getTime()) return true;   
@@ -2598,7 +2655,7 @@ var SPHistory = new TSPHistory();
 
 var speechlevel=0;
 
-function robot(command){
+function SpeechToInputbox(command){
   if ((document.activeElement!=null)){
     if ((document.activeElement.nodeName=="INPUT") ||  ((document.activeElement.nodeName=="TEXTAREA"))){
       var s = document.activeElement.value;
@@ -2607,7 +2664,10 @@ function robot(command){
       return;
     }
   }
+}
 
+function robot(command){
+  //SpeechToInputbox(command);
   command=command.toLowerCase();
   for (let i = 0; i < change.length; i++) {
     const chg = change[i];
@@ -2677,6 +2737,7 @@ function robot(command){
     processSpeech(mini,command,minv,minparams);
   } else {
     console.log(minv,command);
+    SpeechToInputbox(command);//end if INPUT
     if (!SPHistory.ellapsed()){
       command=command.split(" ")[0]; //only one word
       switch (SPHistory.mode){
@@ -2701,6 +2762,9 @@ function processSpeech(idx,command,minv,minparams=null){
   }
   if (speechlevel>0){
     cases=speechlevel;    
+  }
+  if (cases<999){
+    SpeechToInputbox(command);
   }
   switch (cases){
     case 1: //TODO Tablemaker
@@ -2759,19 +2823,20 @@ function processSpeech(idx,command,minv,minparams=null){
       break;
     case 19:
       SP_tabledatasheet(idx,command,minv,minparams);
-    break;
-
+      break;
     case 20:
-      SP_fielddatasheet();
-    break;
+      SP_fielddatasheet(idx,command,minv,minparams);
+      break;
 
-    case 1000:
+    //Browse
+    case 1000: 
       SP_adatfelvitel_new();
       break;
     case 1001:
       SP_adatfelvitel_exit();
       break;
 
+    //Table Edit
       case 2000:
       SP_focusInput("edit_name");
       break;
@@ -2782,7 +2847,8 @@ function processSpeech(idx,command,minv,minparams=null){
       SP_focusInput("edit_height");
       break;
       case 2003:
-      SP_focusInput("edit_color");
+        SP_focusInput("edit_color");
+        SP_changecolor("edit_color",idx,command,minv,minparams)
       break;
       case 2004:
       SP_focusInput("edit_description");
@@ -2793,13 +2859,78 @@ function processSpeech(idx,command,minv,minparams=null){
       case 2006:
       SP_setCheckbox("edit_readonly",false);
       break;
-      
+
+      //Field Edit
+      case 3000:
+      SP_focusInput("edit_field");
+      break;
+      case 3001:
+      SP_focusInput("edit_length");
+      break;
+      case 3002:
+      SP_focusInput("edit_type");
+      break;
+      case 3004:
+      SP_focusInput("edit_description");
+      break;
+      case 3005:
+      SP_setCheckbox("edit_display",true);
+      break;
+      case 3006:
+      SP_setCheckbox("edit_display",false);
+      break;
+      case 3007:
+      SP_deleteLink();
+      break;
+      case 3008:
+      SP_deleteField();
+      break;
+    
     break;
     default:
-      
+      SpeechToInputbox(command);
       break;
   }
   Save(temp);
+}
+
+function SP_deleteField(){
+  if (SelectedField){        
+    SelectedField.destroy();
+    SP_adatfelvitel_exit();
+  }
+}
+
+
+function SP_deleteLink(params) {
+  if (SelectedField){    
+    SelectedField.deleteLink();
+    SP_adatfelvitel_exit();    
+  }
+}
+
+
+
+function SP_changecolor(inputDOM,idx,cmd,minv,minparams) {
+  if (SelectedTable){
+    cmd=cmd.replace(commands[commandgroup][idx].command,"");
+    cmd=cmd.trim().replace(/ /g,"");
+    var div = document.getElementById(inputDOM);
+    if (cmd=="") {
+      setTimeout(function(){
+        div.style.opacity=0;
+        div.click();
+        div.style.opacity=1;
+      },1000);
+      
+      return;
+    }
+    var div = document.getElementById(inputDOM);
+    var col = colourNameToHex(cmd);
+    if (col){
+      div.value=col;
+    }
+  }
 }
 
 function SP_setCheckbox(id,logic) {
@@ -2818,7 +2949,7 @@ function SP_tabledatasheet(idx,command,minv,minparams){
   }
   if (SelectedTable){
     SelectedTable.edit(document.getElementById("area"));
-    commandgroup=2;  
+    //commandgroup=2;  
    }  
 }
 
@@ -2854,7 +2985,7 @@ function SP_showtable(idx,command,minv,minparams){
 
 function SP_adatfelvitel(params) {
   if (SelectedTable){
-    commandgroup=1;    
+    //commandgroup=1;    // in browse function
     SelectedTable.browse(document.getElementById("area"));
   }  
 }
@@ -2868,7 +2999,13 @@ function SP_adatfelvitel_new(){
 }
 
 function SP_adatfelvitel_exit() {
-  editTableCancel(document.getElementById("list").childNodes[0]);
+  if (commandgroup==1){
+    editCancel(document.getElementById("list").childNodes[0]);
+  }
+  if ((commandgroup==2) || (commandgroup==3)){
+    var div = document.getElementById("flow_edit");
+    editCancel(div.childNodes[0]);
+  }
   commandgroup=0;  
 }
 
